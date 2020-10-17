394ee544be10a3bb7221e865fdcf25d70b3b9d5f
Ícaro Rezende <contato@icarorezende.com>
2020-10-17 00:22:02 -0300

    Curso 2002 - Versão Inicial

---
+++ 2002/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vezes = 10;
+            for (int i = 1; i < vezes ; i++)
+            {
+                Console.WriteLine($"Contando... {i} de {vezes}");
+            }
+        }
+    }
+}
